c51882d3214d9b42c52d1094c55a43d3f1567b18
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-14 18:57:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+};
+
+getFirstSelector(div)
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+};
+
+
+function deepestChild(){
+  
+};
+
+function increaseRank(n){
+  
+};
